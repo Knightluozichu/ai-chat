9478c7ac42e78bf526bc7af561ec7e4882403135
hellsing <rainknightpox@gmail.com>
2025-02-12 14:56:40 +0800

    database

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://fmqreoeqzqdaqdtgqzkc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtcXJlb2VxenFkYXFkdGdxemtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzMTc0NDcsImV4cCI6MjA1NDg5MzQ0N30.TtiwMoyWq2QWvmZNdzRMf3pf-DbCV6qaFS-uRnpEwu4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
